c6e7b370fe9f57f4dec710f47a63fc4cee250cd4
luisc <lewsdev@gmail.com>
2023-11-17 13:57:48 -0500

    feature: Add serializers and API view functions

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView, DestroyAPIView
+from .serializers import MenuSerializer
+from .models import Menu
 
 # Create your views here.
 def index(request):
     return HttpResponse("test")
+
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveDestroyAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
