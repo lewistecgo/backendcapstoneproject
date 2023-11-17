65218aa40c42c54e84b50c033aa097e1340c182a
luisc <lewsdev@gmail.com>
2023-11-17 12:29:28 -0500

    feature: Setting models and database connection, Adding test route index

--- backendcapstoneproject/settings.py
+++ backendcapstoneproject/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = "backendcapstoneproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "capstonereservations",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "3306",
+
     }
 }
 
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=6)
+    booking_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} : {self.booking_date}'
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=5)
+
+    def __str__(self):
+        return f'{self.title}: {self.price}'
